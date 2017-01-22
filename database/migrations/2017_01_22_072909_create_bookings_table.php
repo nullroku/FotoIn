bc2496541da328e8a4de7e397e8fb037f14d73f8
nullroku <nullroku@gmail.com>
2017-01-22 16:15:22 +0800

    Bookings
    pembuatan bookings migration, model, controller

---
+++ database/migrations/2017_01_22_072909_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('users_id')->unsigned();//costumer id
+            $table->integer('package_id')->unsigned();
+            $table->string('bank_account_number');
+            $table->string('bank_account_name');
+            $table->string('bank');
+            $table->string('status_payment')->nullable();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
