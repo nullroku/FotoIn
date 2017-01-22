e44ca61dc5d24821a4ebd42c8665d770c63e5a25
nullroku <nullroku@gmail.com>
2017-01-22 15:05:59 +0800

    Package
    pembuatan migration, controller, model packege

---
+++ database/migrations/2017_01_22_064839_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pckg_categories_id')->unsigned();
+            $table->integer('users_id')->unsigned();//photographer id
+            $table->string('duration');
+            $table->string('duration_unit');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
